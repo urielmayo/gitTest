7fe3cebee12ca4872d26ebad801fb1b3efb76540
urielmayo <urimayo00@gmail.com>
2021-04-08 18:04:29 -0300

    agregando funcion calculadora

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ y = 10
 print(mathOps.sum(x,y))
 
 #agregando resto de calculos al programa
+
+number1 = float(input("ingresar primer numero: "))
+operation = input("ingresar operacion: ")
+number2 = float(input("ingresar segundo numero: "))
+
+status, result = mathOps.calculate(number1,number2,operation)
+
+if status:
+    print(f"{number1} {operation} {number2} = {result}")
--- mathOps.py
+++ mathOps.py
@@ -9,3 +9,15 @@ def prod(x,y):
 
 def div(x,y):
 	return x/y
+
+def calculate(x1,x2,operation):
+	if operation == '+':
+		return True, sum(x1, x2)
+	elif operation == '-':
+		return True, sub(x1,x2)
+	elif operation == 'x':
+		return True, prod(x1,x2)
+	elif operation == '/':
+		return True, div(x1,x2)
+	else:
+		return False
